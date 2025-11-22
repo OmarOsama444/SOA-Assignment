a2eb638351214e03a4655367efc41aeeb1eae346
OmarOsamaWT <omar.osama@tildetech.ae>
2025-11-22 17:08:57 +0200

    update db connection library

--- Customer Service/app.py
+++ Customer Service/app.py
@@ -1,10 +1,52 @@
-from flask import Flask , render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask , render_template , jsonify
+import requests;
+import mysql.connector
+import os
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+db_user = os.getenv("MYSQL_USER" , "myuser")
+db_password = os.getenv("MYSQL_PASSWORD", "mypassword")
+db_host = os.getenv("MYSQL_HOST", "localhost")
+db_port = os.getenv("MYSQL_PORT", "3306")
+db_name = os.getenv("MYSQL_DATABASE", "ecommerce_system")
+
+order_service_url=os.getenv("ORDER_SERVICE_URL" , "http://localhost:5001")
+customer_service_url=os.getenv("CUSTOMER_SERVICE_URL" , "http://localhost:5002")
+inventory_service_url=os.getenv("INVENTORY_SERVICE_URL" , "http://localhost:5003")
+notification_service_url=os.getenv("NOTIFICATION_SERVICE_URL" , "http://localhost:5004")
+pricing_service_url=os.getenv("PRICING_SERVICE_URL","http://localhost:5005")
+
+@app.route("/api/customers/<int:customer_id>" , methods=["GET"])
+def GetCustomerProfile(customer_id):
+    try :
+        conn = mysql.connector.connect(
+            host=db_host,
+            port=db_port,
+            user=db_user,
+            password=db_password,
+            database=db_name
+        )
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM customers WHERE customer_id = %s", (customer_id,))
+        customer = cursor.fetchone()
+    except mysql.connector.Error as err:
+        return jsonify({"message": "Database error", "error": str(err)}), 500
+    finally:
+        cursor.close()
+        conn.close()
+    if customer:
+        return jsonify(customer), 200
+    else:
+        return jsonify({"message": "Customer not found"}), 404
+    
+@app.route("api/customers/<int:customer_id>/orders" , methods=["GET"])
+def GetCustomerOrders(customer_id):
+    response = requests.get(f"{order_service_url}/api/orders/customer/{customer_id}")
+    if response.status_code != 200:
+        return jsonify({"message": "Failed to fetch orders from Order Service"}), 500
+    dummy_orders = response.json()
+    return jsonify(dummy_orders), 200
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0", port=5000)
--- Order Service/app.py
+++ Order Service/app.py
@@ -1,7 +1,8 @@
 from flask import Flask , request , jsonify
 from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+from mysql.connector import Error
 from request_schemas import *
-from models import *
 from marshmallow import ValidationError
 import os
 app = Flask(__name__)
@@ -11,6 +12,12 @@ db_password = os.getenv("MYSQL_PASSWORD", "mypassword")
 db_host = os.getenv("MYSQL_HOST", "localhost")
 db_port = os.getenv("MYSQL_PORT", "3306")
 db_name = os.getenv("MYSQL_DATABASE", "ecommerce_system")
+
+order_service_url=os.getenv("ORDER_SERVICE_URL" , "http://localhost:5001")
+customer_service_url=os.getenv("CUSTOMER_SERVICE_URL" , "http://localhost:5002")
+inventory_service_url=os.getenv("INVENTORY_SERVICE_URL" , "http://localhost:5003")
+notification_service_url=os.getenv("NOTIFICATION_SERVICE_URL" , "http://localhost:5004")
+pricing_service_url=os.getenv("PRICING_SERVICE_URL","http://localhost:5005")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -26,16 +33,46 @@ def create_order():
 
 @app.route("/api/orders/<int:order_id>" , methods=["GET"])
 def get_order(order_id):
-    dummy_order = {
-        "order_id": order_id,
-        "customer_id": 123,
-        "products": [
-            {"product_id": 1, "quantity": 2},
-            {"product_id": 2, "quantity": 1}
-        ],
-        "total_amount": 59.99
-    }
-    return jsonify(dummy_order), 200
+    try :
+        conn = mysql.connector.connect(
+            host=db_host,
+            port=db_port,
+            user=db_user,
+            password=db_password,
+            database=db_name
+        );
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM orders WHERE order_id = %s", (order_id,))
+        order = cursor.fetchone()
+    except mysql.connector.Error as err:
+        return jsonify({"message": "Database error", "error": str(err)}), 500
+    finally:
+        cursor.close()
+        conn.close()
+    if not order:
+        return jsonify({"message": "Order not found"}), 404
+    else:
+        return jsonify(order), 200
+    
+@app.route("/api/orders/customer/<int:customer_id>" , methods=["GET"])
+def get_orders_by_customer(customer_id):
+    try :
+        conn = mysql.connector.connect(
+            host=db_host,
+            port=db_port,
+            user=db_user,
+            password=db_password,
+            database=db_name
+        )
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM orders WHERE customer_id = %s", (customer_id,))
+        orders = cursor.fetchall()
+    except mysql.connector.Error as err:
+        return jsonify({"message": "Database error", "error": str(err)}), 500
+    finally:
+        cursor.close()
+        conn.close()
+    return jsonify(orders), 200
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0", port=5000)
